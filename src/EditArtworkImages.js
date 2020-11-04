f0474813823405f7f1fb38cb760270c782776050
Scim <ryanedmondbeck@gmail.com>
2020-11-04 13:44:28 -0600

    can now delete image from storage and db

--- src/EditArtworkImages.js
+++ src/EditArtworkImages.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { db, storage } from './firebase';
 
 function EditArtworkImages({ collID, artID, images, urls }) {
     
+    const handleDelete = async (i) => {
+        const new_images = [];
+        const new_urls = [];
+        for (let j = 0; j < urls.length; j++) {
+            if (i != j) {
+                new_images.push(images[j]);
+                new_urls.push(urls[j]);
+            }
+        }
+        console.log("deleting references to " + images[i]);
+        // update image name and download reference arrays in db
+        try {
+            const res = await db
+            .collection('collection_list')
+            .doc(collID)
+            .collection('collection')
+            .doc(artID)
+            .set({
+                images: new_images,
+                imurl: new_urls
+            }, { merge: true });
+            // console.log(res);
+        } catch (error) { console.log(error); }    
+        
+        console.log("deleting " + images[i] + " from storage.");
+        // delete image from storage
+        storage.ref(images[i]).delete()
+            .then(() => {
+                console.log(images[i] + " deleted from storage");
+            })
+            .catch((error) => {
+                console.log(error);
+            }); 
+    }
+
     const renderImages = () => {
         const image_list = [];
         for (let i in urls) {
@@ -10,6 +46,7 @@ function EditArtworkImages({ collID, artID, images, urls }) {
                 <div key={i}>
                     <p>{images[i]}</p>
                     <img src={urls[i]} alt="" />
+                    <button onClick={() => handleDelete(i)}>Delete</button>
                 </div>
             )
         }
