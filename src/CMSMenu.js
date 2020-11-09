3289824a61693c27cbb9aca06f2cfd10508478f9
Scim <ryanedmondbeck@gmail.com>
2020-11-09 16:03:52 -0600

    cms selected page is underlined CSS

--- src/CMSMenu.js
+++ src/CMSMenu.js
@@ -5,9 +5,15 @@ function CMSMenu() {
     const [cmsPage, setCmsPage] = useContext(CMSPageContext);
     return (
         <div className="cms-menu">
-            <button onClick={() => setCmsPage('artwork')}>Upload New Artwork</button>
-            <button onClick={() => setCmsPage('collection')}>Create New Collection</button>
-            <button onClick={() => setCmsPage('edit')}>Edit Artwork and Collections</button>
+            <button onClick={() => setCmsPage('artwork')}
+            className={`cms-menu__button ${(cmsPage === 'artwork') ? "cms-menu__button--selected" : ""}`}>
+                Upload New Artwork</button>
+            <button onClick={() => setCmsPage('collection')}
+            className={`cms-menu__button ${(cmsPage === 'collection') ? "cms-menu__button--selected" : ""}`}>
+                Create New Collection</button>
+            <button onClick={() => setCmsPage('edit')}
+            className={`cms-menu__button ${(cmsPage === 'edit') ? "cms-menu__button--selected" : ""}`}>
+                Edit Artwork and Collections</button>
         </div>
     )
 }
