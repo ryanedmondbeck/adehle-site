ea29a4462c6dd3f30d2765aab5c46dc2327f3042
Scim <ryanedmondbeck@gmail.com>
2021-01-15 14:09:13 -0600

    cleared warning

--- src/mobile/SplashM.js
+++ src/mobile/SplashM.js
@@ -2,29 +2,26 @@ import React from 'react';
 import './SplashM.css';
 import { Link } from "react-router-dom";
 import SketchSplash from '../SketchSplash';
-import Div100vh from 'react-div-100vh';
 
 function Splash({ setTransition }) {
     return (
-        <Div100vh>
-            <div className="splash">
-                <div className="splash__title">
-                    <p>ADEHLE DALEY</p>
-                </div>
-                <div className="splash__about">
-                    <Link onClick={() => setTransition('sta')} className="splash__button" to="/about-mobile">ABOUT</Link>
-                </div>
-                <div className="splash__portfolio">
-                    <Link onClick={() => setTransition('stp')} className="splash__button" to="/portfolio-mobile">PORTFOLIO</Link>
-                </div>
-                <div className="splash__explore">
-                    <Link className="splash__button" to="/">EXPLORE</Link>
-                </div>     
-                <div className="splash__p5">
-                    <SketchSplash />
-                </div>
+        <div className="splash">
+            <div className="splash__title">
+                <p>ADEHLE DALEY</p>
             </div>
-        </Div100vh>
+            <div className="splash__about">
+                <Link onClick={() => setTransition('sta')} className="splash__button" to="/about-mobile">ABOUT</Link>
+            </div>
+            <div className="splash__portfolio">
+                <Link onClick={() => setTransition('stp')} className="splash__button" to="/portfolio-mobile">PORTFOLIO</Link>
+            </div>
+            <div className="splash__explore">
+                <Link className="splash__button" to="/">EXPLORE</Link>
+            </div>     
+            <div className="splash__p5">
+                <SketchSplash />
+            </div>
+        </div>
     )
 }
 
